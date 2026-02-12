83739f7e152073540749d83c12d473f414de979d
youhan200203 <77678072+youhan200203@users.noreply.github.com>
2026-02-12 08:23:07 -0800

    [Bronze III] Title: 별 찍기 - 17, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10992. 별 찍기 － 17/별 찍기 － 17.py
@@ -0,0 +1,10 @@
+n = int(input())
+for i in range(1, n+1):
+    if i == 1:
+        print(' '*(n-i)+'*'*i)
+    elif i == n:
+        print('*'*(2*n-1))
+    else:
+        print(' '*(n-i)+'*'+' '*(2*i-3)+'*')
+
+#2n-1-(n-i)-(n-i)-2 = 2i-4
