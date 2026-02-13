f4e5abf5432c154faf187e77ea5a03331bcd0520
youhan200203 <77678072+youhan200203@users.noreply.github.com>
2026-02-13 00:48:11 -0800

    [Silver I] Title: 포도주 시식, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,11 +1,13 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 wine = []
 for i in range(n):
     wine.append(int(input()))
 d = [-1]*n 
 
-idx= 0
-while idx < n:
+for idx in range(n):
     if idx == 0:
         d[idx] = wine[0]
     elif idx == 1:
@@ -17,3 +19,6 @@ while idx < n:
     idx+=1
 
 print(d[n-1])
+
+#많이 헤맸다. 우선 어차피 모든 d[i]를 계산해야 한다면, top-down 함수를 쓸 이유가 없고 bottom-up으로 올라가야 recursion error가 뜨지 않는다.
+#또한 바로 전 열에서 +하고 그러는 게 아니라 그냥 깔끔하게 상태 개수만큼 뺀 열에서 경우의 수를 나누자
