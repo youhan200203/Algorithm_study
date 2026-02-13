54165d0a7886b1681582b240e22de1d1ce551f22
youhan200203 <77678072+youhan200203@users.noreply.github.com>
2026-02-12 18:46:05 -0800

    [Silver III] Title: 1로 만들기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,12 +1,11 @@
 n = int(input())
-d=[-1]*(n+5)
-d[1]=0
-d[2]=1
-d[3]=1
+d = {1:0, 2:1, 3:1}
 def dp(idx):
-    if d[idx] != -1:
+    if idx in d:
         return d[idx]
     d[idx] = 1 + min(dp(idx//3)+idx%3, dp(idx//2)+idx%2)
     return d[idx]
 
 print(dp(n))
+
+#매번 -1을 해주기보다는, 나머지를 이용해 규칙을 만들어내 수학적으로 빠르게!
