8c8b573c3db789e46c2df9125f6230284fd5fe65
youhan200203 <77678072+youhan200203@users.noreply.github.com>
2026-03-03 01:36:12 -0800

    Delete 1174-immediate-food-delivery-ii directory

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++
@@ -1,3 +0,0 @@
-SELECT ROUND(SUM(DATEDIFF(order_date, customer_pref_delivery_date) = 0)*100/COUNT(*), 2) AS immediate_percentage
-FROM Delivery d
-WHERE order_date = (SELECT MIN(order_date) FROM Delivery WHERE customer_id = d.customer_id)
