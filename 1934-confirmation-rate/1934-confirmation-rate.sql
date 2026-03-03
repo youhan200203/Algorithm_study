7a6f891fe61b2aa924cbf68ba7f0575c54b4c258
youhan200203 <77678072+youhan200203@users.noreply.github.com>
2026-03-02 23:29:23 -0800

    Delete 1934-confirmation-rate directory

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++
@@ -1,5 +0,0 @@
-SELECT s.user_id, ROUND(IFNULL(SUM(c.action='confirmed')/COUNT(c.action), 0), 2) AS confirmation_rate
-FROM Signups s
-LEFT JOIN Confirmations c
-ON s.user_id = c.user_id
-GROUP BY s.user_id
