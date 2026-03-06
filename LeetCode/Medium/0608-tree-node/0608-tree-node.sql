7fc1781855f8a50e85a5e96a3f9299fa33e16562
youhan200203 <77678072+youhan200203@users.noreply.github.com>
2026-03-06 01:54:11 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0608-tree-node/0608-tree-node.sql
+++ LeetCode/Medium/0608-tree-node/0608-tree-node.sql
@@ -1,7 +1,4 @@
 #LEFT JOIN하면 될 거 같은뎅? a.id = b.p_id가 되면 a가 null이면 a를 p_id로 가지는 게 없다는 거니까 자식
 
-SELECT a.id, (CASE WHEN SUM(a.p_id) IS NULL THEN 'Root' WHEN SUM(b.id) IS NULL THEN 'Leaf' ELSE 'Inner' END) AS 'type'
-FROM Tree a
-LEFT JOIN Tree b
-ON a.id = b.p_id
-GROUP BY a.id
+SELECT id, CASE WHEN p_id IS NULL THEN 'Root' WHEN id IN (SELECT p_id FROM Tree) THEN 'Inner' ELSE 'Leaf' END AS 'type'
+FROM Tree
