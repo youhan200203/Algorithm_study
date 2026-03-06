707b4f3d200a655a137e6f040924ef1d03ec9cf8
youhan200203 <77678072+youhan200203@users.noreply.github.com>
2026-03-05 23:49:14 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0585-investments-in-2016/0585-investments-in-2016.sql
+++ LeetCode/Medium/0585-investments-in-2016/0585-investments-in-2016.sql
@@ -1,22 +1,18 @@
-#tiv_2016의 sum when tiv_2015가 같은 또다른 pid가 있고 lat,lon이 unique
-
-WITH cte AS (
-    SELECT a.pid, a.tiv_2016
-    FROM Insurance a
-    LEFT JOIN Insurance b
-    ON a.tiv_2015 = b.tiv_2015 AND a.pid != b.pid
-    WHERE b.pid IS NOT NULL
-    GROUP BY a.pid
+WITH tiv AS (
+    SELECT tiv_2015
+    FROM Insurance
+    GROUP BY tiv_2015
+    HAVING COUNT(*) > 1
 ),
 
-cte2 AS (
-    SELECT pid, tiv_2015, tiv_2016 
+latlon AS (
+    SELECT lat, lon
     FROM Insurance
     GROUP BY lat, lon
     HAVING COUNT(*) = 1
 )
 
-SELECT ROUND(SUM(c.tiv_2016), 2) AS tiv_2016
-FROM cte2 c
-JOIN cte d
-ON c.pid = d.pid
+SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
+FROM Insurance
+WHERE tiv_2015 in (SELECT tiv_2015 FROM tiv)
+AND (lat, lon) in (SELECT lat, lon FROM latlon)
