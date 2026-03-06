eacf7e76c1e261538b55bf5cc6bdc90dd685af21
youhan200203 <77678072+youhan200203@users.noreply.github.com>
2026-03-06 00:06:31 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0176-second-highest-salary/0176-second-highest-salary.sql
+++ LeetCode/Medium/0176-second-highest-salary/0176-second-highest-salary.sql
@@ -2,6 +2,6 @@
 WITH cte AS (SELECT *, DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk
 FROM Employee)
 
-SELECT MAX(DISTINCT salary) AS SecondHighestSalary
+SELECT MAX(salary) AS SecondHighestSalary
 FROM cte 
 WHERE rnk = 2
